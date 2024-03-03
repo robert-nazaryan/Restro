0376a5e560549adce555d834087d2d7864cb43fa
Robert Nazaryan <robertnazaryan0@gmail.com>
2024-03-03 17:48:22 +0400

    Initial commit

---
+++ src/test/java/org/restro/RestroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.restro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
